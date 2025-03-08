32ef3f937b2605beda256ddd75a512616e57afc8
Not Zero <32226366+YSpoof@users.noreply.github.com>
2025-03-08 15:24:46 -0300

    Initial files/template for HomePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent() {
+      return import('./pages/home/home.component').then(
+        (m) => m.HomePageComponent
+      );
+    },
+  },
+];
---
+++ src/app/components/home/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  template: `
+    <footer class="bg-bcrblack p-4 w-full">
+      <div class="container mx-auto text-center">
+        <p class="text-white">
+          Desenvolvido por <a href="#">Igor</a> e <a href="#">Luís</a>
+        </p>
+      </div>
+    </footer>
+  `,
+})
+export class FooterComponent {}
